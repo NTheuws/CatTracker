a480a19a8d937499d896deafd4c0b7f29e41fbe0
Niels <niels.theuws@student.fontys.nl>
2022-01-13 14:30:41 +0100

    functions added

--- LaserPointer/LaserPointer.ino
+++ LaserPointer/LaserPointer.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
 
+// Pins for the servos and LED.
 #define SERVOX 9
 #define SERVOY 10
 #define LED 13
@@ -10,39 +11,177 @@ int _angleX = 90; //X current angle.
 int _angleY = 90; //Y current angle.
 
 int lastMsg = 0; // Last corner msg received.
+int randomNum = 0; // Random value for events.
+
+// 3 points for the y-axis.
+const int topY = 108;
+const int middleY = 90;
+const int bottomY = 72;
+
+// 3 points for the x-axis.
+const int leftX = 114;
+const int middleX = 90;
+const int rightX = 66;
 
 void setup() {
   _servoX.attach(SERVOX);
   _servoY.attach(SERVOY);
   pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
+  digitalWrite(LED, LOW); // Make sure LED is turned off before it starts.
   Serial.begin(9600);
 }
 
 void loop() {
+
   String receivedMessage = readSerial();
-  // Top left.
-  if (receivedMessage == "1" && lastMsg != 1)
+
+  // Turn the LED on when anything is received.
+  if (receivedMessage != "")
   {
-    moveServo(_angleX, _angleY, 115, 115, _servoX, _servoY);
-    lastMsg = 1;
+    digitalWrite(LED, HIGH);
   }
-  // Bottom left.
-  if (receivedMessage == "2" && lastMsg != 2)
+  
+  /*  There is a 3x3 grid, which means there are 9 squares.
+   *  The Pointer will be directed towards one of these.
+   *  These squares are named as followed:
+   * 
+   *1: Top Left. 
+   *2: Top Middle.
+   *3: Top Right.
+   *4. Middle Left.
+   *5: Middle Middle.
+   *6: Middle Right.
+   *7: Bottom Left.
+   *8: Bottom Middle.
+   *9: Bottom Right.
+   */
+  switch (receivedMessage.toInt()) {
+    case 1:
+      if (lastMsg  != 1)
+      {
+        moveToPoint(leftX, topY, 1);
+      }
+      break;
+    case 2:
+      if (lastMsg  != 2)
+      {
+        moveToPoint(middleX, topY, 2);
+      }
+      break;
+    case 3:
+      if (lastMsg  != 3)
+      {
+        moveToPoint(rightX, topY, 3);
+      }
+      break;
+    case 4:
+      if (lastMsg  != 4)
+      {
+        moveToPoint(leftX, middleY, 4);
+      }
+      break;
+    case 5:
+      if (lastMsg  != 5)
+      {
+        moveToPoint(middleX, middleY, 5);
+      }
+      break;
+    case 6:
+      if (lastMsg  != 6)
+      {
+        moveToPoint(rightX, middleY, 6);
+      }
+      break;
+    case 7:
+      if (lastMsg  != 7)
+      {
+        moveToPoint(leftX, bottomY, 7);
+      }
+      break;
+    case 8:
+      if (lastMsg  != 8)
+      {
+        moveToPoint(middleX, bottomY, 8);
+      }
+      break;
+    case 9:
+      if (lastMsg  != 9)
+      {
+        moveToPoint(rightX, bottomY, 9);
+      }
+      break;
+  }
+
+  // When waiting blink 25% - 1/4 chance.
+  if (randomNum % 4 == 1)
   {
-    moveServo(_angleX, _angleY, 115, 65, _servoX, _servoY);
-    lastMsg = 2;
+    delay(240);
+    digitalWrite(LED, LOW);
+    delay(240);
+    digitalWrite(LED, HIGH);
   }
-  // Top right.
-  if (receivedMessage == "3" && lastMsg != 3)
+
+  // When waiting move 20% - 1/5 chance.
+  else if (randomNum % 5 == 1)
   {
-    moveServo(_angleX, _angleY, 65, 115, _servoX, _servoY);
-    lastMsg = 3;
+    idleMovement(_angleX, _angleY, _servoX, _servoY);
   }
-  // Bottom right.
-  if (receivedMessage == "4" && lastMsg != 4)
+}
+
+// Move the laser dot to a new position with a chance of making a fake movement.
+void moveToPoint(int posX, int posY, int msg)
+{
+  nextRandom();
+
+  // Chance to pretend to go to a different square.
+  if (randomNum % 2 == 1)
   {
-    moveServo(_angleX, _angleY, 65, 65, _servoX, _servoY);
-    lastMsg = 4;
+    fakeMovement();
   }
+
+  // Move the laser dot.
+  moveServo(_angleX, _angleY, posX, posY, _servoX, _servoY);
+  lastMsg = msg;
+}
+
+// Prentend the square went to another corner.
+void fakeMovement()
+{
+  int val = random(1, 10);
+  switch (val) {
+    case 1:
+      moveServo(_angleX, _angleY, 60, 114, _servoX, _servoY);
+      break;
+    case 2:
+      moveServo(_angleX, _angleY, 90, 114, _servoX, _servoY);
+      break;
+    case 3:
+      moveServo(_angleX, _angleY, 120, 114, _servoX, _servoY);
+      break;
+    case 4:
+      moveServo(_angleX, _angleY, 60, 90, _servoX, _servoY);
+      break;
+    case 5:
+      moveServo(_angleX, _angleY, 90, 90, _servoX, _servoY);
+      break;
+    case 6:
+      moveServo(_angleX, _angleY, 120, 90, _servoX, _servoY);
+      break;
+    case 7:
+      moveServo(_angleX, _angleY, 60, 66, _servoX, _servoY);
+      break;
+    case 8:
+      moveServo(_angleX, _angleY, 90, 66, _servoX, _servoY);
+      break;
+    case 9:
+      moveServo(_angleX, _angleY, 120, 66, _servoX, _servoY);
+      break;
+  }
+}
+
+// Generate next random number between 1-100.
+// This number is used to make random events happen.
+void nextRandom()
+{
+  randomNum = random(1, 101);
 }
--- LaserPointer/Servo.ino
+++ LaserPointer/Servo.ino
@@ -1,15 +1,77 @@
+// Moving the servo to a specific corner.
 void moveServo(int angleX, int angleY, int goalX, int goalY, Servo servoX, Servo servoY)
 {
-  int valX = (goalX - angleX) / 10;
-  int valY = (goalY - angleY) / 10;
-  for ( int num = 0; num < 10; num++)
+  // Speed randomizer.
+  int randomValue = random(5, 60);
+  bool blinking = false;
+
+  if (randomValue % 3 == 0)
   {
-    servoX.write(angleX + valX * num);
-    servoY.write(angleY + valY * num);
-    delay(30);
+    blinking = true;
   }
 
-  // moving directly to the goal instead of curving
-  //_angleX = goalX;
-  //_angleY = goalY;
+  double valX = (goalX - angleX) / 24;
+  double valY = (goalY - angleY) / 18;
+  for ( int num = 0; num < 24; num++)
+  {
+    // Prevent it from blinking too fast. Only blink in slower movements.
+    if (blinking && randomValue > 30)
+    {
+      if (num % 6 == 0)
+      {
+        digitalWrite(LED, LOW);
+      }
+      else if (num % 3 == 0)
+      {
+        digitalWrite(LED, HIGH);
+      }
+    }
+
+    // Move both axis to the correct point.
+    servoX.write(angleX + valX * num);
+
+    if (num < 18)
+    {
+      servoY.write(angleY + valY * num);
+    }
+    delay(randomValue);
+  }
+
+  // moving directly to the goal instead of curving from the center.
+  _angleX = angleX + valX * 24;
+  _angleY = angleY + valY * 18;
+  delay(300);
+}
+
+// IDLE movement. Moving around in squares.
+void idleMovement(int angleX, int angleY, Servo servoX, Servo servoY)
+{
+  servoX.write(_angleX);
+  servoY.write(_angleY);
+
+  // Going a around in 4 steps.
+  for (int num = 0; num < 20; num++)
+  {
+    if (num < 5) // First 5, 0-4.
+    {
+      _angleX += 2;
+      servoX.write(_angleX);
+    }
+    else if (num > 4 && num < 10) // Second 5, 5-9.
+    {
+      _angleY += 2;
+      servoY.write(_angleY);
+    }
+    else if (num > 9 && num < 15) // Third 5, 10-14.
+    {
+      _angleX += -2;
+      servoX.write(_angleX);
+    }
+    else if (num < 20) // Fourth 5, 15-19.
+    {
+      _angleY += -2;
+      servoY.write(_angleY);
+    }
+    delay(50);
+  }
 }
